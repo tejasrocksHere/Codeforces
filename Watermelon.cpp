33a3b540f3707a3146b689839b6c5a55c9f73207
tejas <teja77209986776@gmail.com>
2024-09-20 10:56:42 +0530

    nice

---
+++ Watermelon.cpp
@@ -0,0 +1,19 @@
+// ermelon	 GNU C++0x	Accepted	 15 ms	 0 KB
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    if (w % 2 == 0 && w > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
